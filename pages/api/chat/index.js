126d3453c4c7c1401e89da29ac8966146da4bd6b
hai elimelech <haie@my.hit.ac.il>
2023-08-13 22:46:29 +0300

    Update index.js

--- pages/api/chat/index.js
+++ pages/api/chat/index.js
@@ -1,5 +1,5 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { initializeAgentExecutorWithOptions} from "langchain/agents";
+import { initializeAgentExecutorWithOptions,createVectorStoreRouterAgent} from "langchain/agents";
 import { SerpAPI, ChainTool } from "langchain/tools";
 import { VectorDBQAChain } from "langchain/chains";
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
@@ -8,6 +8,10 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { OpenAI } from "langchain/llms/openai";
 import * as fs from "fs";
 import { Input } from "postcss";
+import {Calculator} from "langchain/tools/calculator";
+import { Console } from "console";
+import { RetrievalQAChain } from "langchain/chains";
+import { PromptTemplate } from "langchain/prompts";
 const path = require('path');
 
 export default async function handler(req,res){
@@ -23,13 +27,14 @@ export default async function handler(req,res){
         callbacks:[
             {
                 handleLLMNewToken(token){
-                    if (token === ' Input') {
-                        shouldStartWriting = true;
-                        token = 'תשובה עבור';               
-                    }
-                    if (shouldStartWriting) {
-                        res.write(token);
-                    } 
+                    res.write(token);
+                    // if (token === ' Input') {
+                    //     shouldStartWriting = true;
+                    //     token = 'תשובה עבור';               
+                    // }
+                    // if (shouldStartWriting) {
+                    //     res.write(token);
+                    // }
                 }
             },
         ]
@@ -40,39 +45,43 @@ export default async function handler(req,res){
     const docs = await textSplitter.createDocuments([text]);
     const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
     const dataChain = VectorDBQAChain.fromLLM(model,vectorStore);
-      
-    const prefix =`You are a helpful AI assistant for trip Caravan company called Tevel Campers. However
-     Answer just in hebrew,
-    every question asked you answer base on the tevel-campers-qa qaTool`;
-        
-    const qaTool = new ChainTool({
-        name: "tevel-campers-qa",
-        description:
-          `אתה שימושי כאשר שואלים אותך על השכרת קרוואנים,מחירים,יעדים ,הצעות מחיר,מידע כללי על הקרוואן,תכנון מסלולים,פרטים ודרכי התקשרות,
-        ..תמיד תשאל אם יש עוד שאלות ותהיה נחמד ואדיב ותענה רק בעברית`,
-        chain: dataChain,
-        returnDirect: true,
-      });
-      
-    const tools = [
-        // new SerpAPI(process.env.SERPAPI_API_KEY,{hl: "en",gl: "us"}),
-        qaTool,
-    ];
 
-    const executer = await initializeAgentExecutorWithOptions(
-        [qaTool],
-        model,
-        {
-        agentType: "zero-shot-react-description",
-        agentArgs:{
-            prefix,
-        },
-    }
-    );
+    const template  = `You are a helpful AI assistant for trip Caravan company called Tevel Campers`; 
+
+    const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+    // const prefix =`You are a helpful AI assistant for trip Caravan company called Tevel Campers. However Answer just in hebrew and 
+    // every question asked you answer base on the tevel-campers-qa qaTool`;
+    
+    // const qaTool = new ChainTool({
+    //     name: "tevel-campers-qa",
+    //     description:`אתה שימושי כאשר שואלים אותך על השכרת קרוואנים,מחירים,יעדים ,הצעות מחיר,מידע כללי על הקרוואן,תכנון מסלולים,פרטים ויצירת קשר,
+    //     ..תמיד תשאל אם יש עוד שאלות ותהיה נחמד ואדיב ותענה רק בעברית`,
+    //     chain: chain,
+    //     returnDirect: true,
+    //   });
+    
+    // const tools = [
+    //     // new SerpAPI(process.env.SERPAPI_API_KEY,{hl: "en",gl: "us"}),
+    //     new Calculator(),
+    //     qaTool,
+    // ];
+
+    // const executer = await initializeAgentExecutorWithOptions(
+    //     [qaTool],
+    //     model,
+    //     {
+    //     agentType: "zero-shot-react-description",
+    //     agentArgs:{
+    //         prefix,
+    //     },
+    // }
+    // );
    
     console.log("Loaded the agent...");
 
-    await executer.run(prompt);
+    // await executer.run(prompt);
+
+    await chain.run(prompt);
 
     res.end();
 }
