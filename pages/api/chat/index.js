e961676e5d2dc9203b89c22c90370a10c0d30595
hai elimelech <hai842@gmail.com>
2023-08-14 12:52:33 +0300

    update

--- pages/api/chat/index.js
+++ pages/api/chat/index.js
@@ -8,12 +8,9 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { OpenAI } from "langchain/llms/openai";
 import * as fs from "fs";
 import { Input } from "postcss";
-<<<<<<< HEAD
 import {Calculator} from "langchain/tools/calculator";
 import { RetrievalQAChain ,loadQAStuffChain  } from "langchain/chains";
 import { PromptTemplate } from "langchain/prompts";
-=======
->>>>>>> parent of 126d345 (Update index.js)
 const path = require('path');
 
 export default async function handler(req,res){
@@ -26,17 +23,17 @@ export default async function handler(req,res){
         modelName:"gpt-3.5-turbo",
         temperature:0.5,
         streaming:true,
-        verbose:true,
         callbacks:[
             {
                 handleLLMNewToken(token){
-                    if (token === ' Input') {
-                        shouldStartWriting = true;
-                        token = 'תשובה עבור';               
-                    }
-                    if (shouldStartWriting) {
-                        res.write(token);
-                    } 
+                    res.write(token);
+                    // if (token === ' Input') {
+                    //     shouldStartWriting = true;
+                    //     token = 'תשובה עבור';               
+                    // }
+                    // if (shouldStartWriting) {
+                    //     res.write(token);
+                    // } 
                 }
             },
         ]
@@ -46,40 +43,22 @@ export default async function handler(req,res){
     const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
     const docs = await textSplitter.createDocuments([text]);
     const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-
     const dataChain = VectorDBQAChain.fromLLM(model,vectorStore);
       
     const prefix =`You are a helpful AI assistant for trip Caravan company called Tevel Campers. However
-     Answer just in hebrew,
+    Answer just in hebrew,
     every question asked you answer base on the tevel-campers-qa qaTool`;
         
     const qaTool = new ChainTool({
         name: "tevel-campers-qa",
-        description:
-          `אתה שימושי כאשר שואלים אותך על השכרת קרוואנים,מחירים,יעדים ,הצעות מחיר,מידע כללי על הקרוואן,תכנון מסלולים,פרטים ודרכי התקשרות,
-        ..תמיד תשאל אם יש עוד שאלות ותהיה נחמד ואדיב ותענה רק בעברית`,
+        description:`שאתה שימושי כאשר שואלים אותך על השכרת קרוואנים,מחירים,יעדים ,הצעות מחיר,מידע כללי על הקרוואן,תכנון מסלולים,פרטים ודרכי התקשרות,Always answer in hebrew, Ask if there any more question every time you finish answering a question`,
         chain: dataChain,
         returnDirect: true,
       });
       
-    const tools = [
-        // new SerpAPI(process.env.SERPAPI_API_KEY,{hl: "en",gl: "us"}),
-        qaTool,
-    ];
-
-<<<<<<< HEAD
     //const retriever = vectorStore.asRetriever();
 
-    const prefix =`Use the following pieces of context to answer the users question. If you don't know the answer, just say that you don't know, don't try to make up an answer`;
-
    // const chain = RetrievalQAChain.fromLLM(model,retriever,{descriptionTemplate});
-    const tool = new ChainTool({
-        name: "tevel-campers-information",
-        description:`אתה שימושי כאשר שואלים אותך על השכרת קרוואנים,מחירים,יעדים ,הצעות מחיר,מידע כללי על הקרוואן,תכנון מסלולים,פרטים ויצירת קשר,
-        ..תמיד תשאל אם יש עוד שאלות ותהיה נחמד ואדיב ותענה רק בעברית`,
-        chain:dataChain,
-        returnDirect:true,
-      });
     
     // const tools = [
     //     // new SerpAPI(process.env.SERPAPI_API_KEY,{hl: "en",gl: "us"}),
@@ -87,34 +66,18 @@ export default async function handler(req,res){
     //     qaTool,
     // ];
 
-    const executer = await initializeAgentExecutorWithOptions(
-        [tool],
-        model,
-        {
-        agentType: "openai-functions",
+    const executer = await initializeAgentExecutorWithOptions([qaTool],new OpenAI({modelName:"gpt-3.5-turbo"}),{
+        agentType:"zero-shot-react-description",
         agentArgs:{
             prefix
-=======
-    const executer = await initializeAgentExecutorWithOptions(
-        [qaTool],
-        model,
-        {
-        agentType: "zero-shot-react-description",
-        agentArgs:{
-            prefix,
->>>>>>> parent of 126d345 (Update index.js)
-        },
-    }
+    }}
     );
    
     console.log("Loaded the agent...");
 
     await executer.run(prompt);
-<<<<<<< HEAD
 
     //await chain.call({query:prompt});
-=======
->>>>>>> parent of 126d345 (Update index.js)
 
     res.end();
 }
