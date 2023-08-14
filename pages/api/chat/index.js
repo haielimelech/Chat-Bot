3d1fe5d16b14dd111dec1572ce24dbb0b6bb6bcb
hai elimelech <hai842@gmail.com>
2023-08-14 13:27:46 +0300

    Update

--- pages/api/chat/index.js
+++ pages/api/chat/index.js
@@ -27,13 +27,6 @@ export default async function handler(req,res){
             {
                 handleLLMNewToken(token){
                     res.write(token);
-                    // if (token === ' Input') {
-                    //     shouldStartWriting = true;
-                    //     token = 'תשובה עבור';               
-                    // }
-                    // if (shouldStartWriting) {
-                    //     res.write(token);
-                    // } 
                 }
             },
         ]
@@ -44,40 +37,47 @@ export default async function handler(req,res){
     const docs = await textSplitter.createDocuments([text]);
     const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
     const dataChain = VectorDBQAChain.fromLLM(model,vectorStore);
-      
-    const prefix =`You are a helpful AI assistant for trip Caravan company called Tevel Campers. However
-    Answer just in hebrew,
-    every question asked you answer base on the tevel-campers-qa qaTool`;
-        
-    const qaTool = new ChainTool({
-        name: "tevel-campers-qa",
-        description:`שאתה שימושי כאשר שואלים אותך על השכרת קרוואנים,מחירים,יעדים ,הצעות מחיר,מידע כללי על הקרוואן,תכנון מסלולים,פרטים ודרכי התקשרות,Always answer in hebrew, Ask if there any more question every time you finish answering a question`,
-        chain: dataChain,
-        returnDirect: true,
-      });
-      
-    //const retriever = vectorStore.asRetriever();
 
-   // const chain = RetrievalQAChain.fromLLM(model,retriever,{descriptionTemplate});
-    
+    const prefix =`You are a helpful AI assistant for trip Caravan company called Tevel Campers. However
+    Answer just in hebrew, and always ask if there any more question every time you finish answering a question`;
+
+    const retriever = vectorStore.asRetriever();
+
+    // console.log("Loaded the agent...");
+
+    const chain = RetrievalQAChain.fromLLM(model,retriever,{
+        agentType:"zero-shot-react-description",
+        verbose:true,
+        agentArgs:{
+            prefix
+        }
+    });
+        
+    // const qaTool = new ChainTool({
+    //     name: "tevel-campers-qa",
+    //     description:`שאתה שימושי כאשר שואלים אותך על השכרת קרוואנים,מחירים,יעדים ,הצעות מחיר,מידע כללי על הקרוואן,תכנון מסלולים,פרטים ודרכי התקשרות,Always answer in hebrew, Ask if there any more question every time you finish answering a question`,
+    //     chain: dataChain,
+    //     returnDirect: true,
+    //   });
+
     // const tools = [
     //     // new SerpAPI(process.env.SERPAPI_API_KEY,{hl: "en",gl: "us"}),
     //     new Calculator(),
     //     qaTool,
     // ];
 
-    const executer = await initializeAgentExecutorWithOptions([qaTool],new OpenAI({modelName:"gpt-3.5-turbo"}),{
-        agentType:"zero-shot-react-description",
-        agentArgs:{
-            prefix
-    }}
-    );
+    // const executer = await initializeAgentExecutorWithOptions([qaTool],new OpenAI({modelName:"gpt-3.5-turbo"}),{
+    //     agentType:"zero-shot-react-description",
+    //     agentArgs:{
+    //         prefix
+    // }}
+    // );
    
-    console.log("Loaded the agent...");
+    // console.log("Loaded the agent...");
 
-    await executer.run(prompt);
+    // await executer.run(prompt);
 
-    //await chain.call({query:prompt});
+    await chain.run(prompt);
 
     res.end();
 }
